366af2040798bc7e90ba63646865c79fc354822c
talryan <nataliamryan@gmail.com>
2020-11-04 16:10:11 -0800

    commit tree 6e775673303c0de91d393841f6ad63e55365dc60

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+    attr_accessor :name, :songs, :genres
+@@all = []
+  
+def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.map {|song| song.genre}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end
+
+
+end 
